041f1690eeee863736288f47f2068823da94999a
Evgeny V. Generalov <e.generalov@gmail.com>
2014-04-11 18:35:00 +0600

    added ISBN http handler

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+var bwipjs = require('./node-bwipjs2');
+var url = require('url');
+
+
+var ZEROGIF_BASE64 = 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+
+module.exports.isbnPNG = function isbnPNG(req, res) {
+    var queryObject = url.parse(req.url, true).query;
+    var isbn = queryObject.text;
+    var args = {
+        bcid: 'isbn',
+        text: isbn,
+        includetext: true,
+        guardwhitespace: true
+    }, png;
+    try {
+        png = bwipjs(args);
+        res.writeHead(200, { 'Content-Type':'image/png' });
+        res.end(png, 'binary');
+    } catch (e) {
+        res.writeHead(400, { 'Content-Type':'image/gif' });
+        res.end(new Buffer(ZEROGIF_BASE64, 'base64').toString('binary'));
+    }
+};
+
